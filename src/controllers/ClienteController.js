1f1accecd4cdbc743865c95ab44b79a5f410f9a7
redmasters <erasmo.cjunior@gmail.com>
2021-08-18 18:19:50 -0300

    Insert Cliente DB Working

--- src/controllers/ClienteController.js
+++ src/controllers/ClienteController.js
@@ -1,4 +1,5 @@
 // Inserir nome do cliente na DB
+// Verificar se nome já existe
 
 const Database = require('../db/config')
 
@@ -6,16 +7,47 @@ module.exports = {
   
     async create(req, res) {
         // Captura o nome do cliente em Index.ejs
-        const cliente = req.body.cliente
-        let clienteId
+        const db = await Database()
+        const clienteName = req.body.cliente
         let isCliente = true
-
-        while (isCliente) {
-            // Gera o nr do Cliente
-            for(var i = 0; i < 6; i++) {
-                i == 0 ? clienteId = i
-            }
+        
+        const verificarNome = await db.all(`
+            SELECT name FROM clientes WHERE name = "${clienteName}"
+        `)
+        
+        isCliente = verificarNome.some(verificarNome => verificarNome === clienteName)
+        console.log('isCliente', isCliente)
+        console.log('VerifyNome', verificarNome)
+        
+        
+        if (! isCliente ) {
+            await db.run(`
+            INSERT INTO clientes (
+                id,
+                name
+            ) VALUES (
+                NULL,
+                "${clienteName}"
+            )
+        `)
+        } else {
+            alert('Já cadastrado')
         }
+        
+        res.redirect(`/cliente/`)
+        
+        await db.close()
+
+    },
+
+    async open(req, res) {
+        const db = await Database();
+        
+        const clientes = await db.all(`
+            SELECT * FROM clientes 
+            `)
+        
+        res.render("cliente", {clientes: clientes})
     }
   
 }
--- src/route.js
+++ src/route.js
@@ -1,8 +1,16 @@
+const { Router } = require('express')
 const express = require('express')
+const ClienteController = require('./controllers/ClienteController')
 
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'pedido'}))
+
+route.get('/', (req, res) => res.render ("index", {page:'cliente'}))
+route.post('/cliente', ClienteController.create)
+route.get('/cliente', ClienteController.open)
+
+        
 
 module.exports = route
