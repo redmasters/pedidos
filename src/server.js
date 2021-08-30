f97a249504ca0d5b3c4b5dcdb3946e4656cdd166
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-08-29 22:08:15 -0300

    tests folder

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const route = require('./route')
-const server = express();
-const port = process.env.PORT || 3000;
-const path = require('path');
+const express = require('express')
+const server = express()
+const router = require('./router')
+const port = 3000
+const path = require('path')
 
-server.set('view engine', 'ejs');
-server.use(express.static("public"));
-server.set('views', path.join(__dirname, 'views'))
+server.use(router)
+server.set('view engine', 'ejs')
+
+server.set('views', path.join(__dirname, './views'))
+server.use(express.static("public"))
+
 server.use(express.urlencoded({extended: true}))
 
-server.use(route)
-
-server.listen(port, () => console.log('rodando na porta:', port, '\n http://localhost:3000'))
+server.listen(port, () => console.log(`Rodando em http://localhost:${port}`))
