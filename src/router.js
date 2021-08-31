58fe0089814e2275ac709fd11e47ee4b10ed299d
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-08-30 21:59:54 -0300

    tests folder

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
+const { render } = require('ejs')
 const express = require('express')
 const router =  express.Router()
 
 // router.get('/', (req, res) => {res.render("index")})
 router.get('/', (req, res) => res.render("index", {page: 'criar-pedido'}))
-router.get('/produtos', (req, res) => {res.render("produtos")})
+router.get('/produtos', (req, res) => {res.render("index")})
+router.get('/pedidos', (req, res) => {res.render("pedidos")})
 
 module.exports = router
